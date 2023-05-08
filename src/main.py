4058ed66f924aadfd8fe5e0d7039ebd3f3434bae
Alexander <alexandr.s.trushnikov@gmail.com>
2023-05-08 12:55:03 +0500

    Cut out multithreading

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
+from multiprocessing import Process, Queue
+
 from loguru import logger
-from queue import Queue
 from pymongo.collection import Collection
 
 from settings import get_settings
@@ -7,17 +8,11 @@ from db import get_db
 from json_parser import JsonParser
 
 
-JSON_PATH = "../test.json"
-THREADS_NUM = 4
+JSON_PATH = "../work.json"
 PRODUCTS_COLLECTION = "products"
 
 
 def main() -> None:
-    """
-    Парсит json-файл в N потоках, 
-    отправляет сформированные объекты в очередь. 
-    Объкты из очереди в БД пишет главный поток.
-    """
     settings = get_settings()
     db = get_db(settings)
     logger.debug(db)
@@ -25,7 +20,7 @@ def main() -> None:
     logger.debug(products_collection)
     queue = Queue()
 
-    parser = JsonParser(json_file=JSON_PATH, max_threads=THREADS_NUM, queue=queue)    
+    parser = JsonParser(json_file=JSON_PATH, queue=queue)    
     parser.run()
 
     got_products = 0
